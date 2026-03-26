30ae85640c0b4bd672d1c214b8b9de8439234221
Bownika <bownikas@gmail.com>
2026-03-26 22:18:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,20 +3,20 @@ import requests
 
 st.title("🌦️ Weather Dashboard")
 
-API_KEY = st.secrets.get("ea60aa32f62e2d9e5fbe7ff6fe5bca2b")
+API_KEY = st.secrets["7e9c04257efca0ae3b5a708436f6403a"]
 
 city = st.text_input("Enter City Name", "Chennai")
 
 if st.button("Get Weather"):
-    if not API_KEY:
-        st.error("API Key not found. Add it in Streamlit Secrets.")
+    if city.strip() == "":
+        st.warning("Please enter a city name")
     else:
         url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
         response = requests.get(url)
         data = response.json()
 
         if data.get("cod") == 200:
-            st.success(f"Weather in {city}")
+            st.success(f"Weather in {data['name']}")
             st.write(f"🌡️ Temp: {data['main']['temp']} °C")
             st.write(f"💧 Humidity: {data['main']['humidity']}%")
             st.write(f"🌬️ Wind: {data['wind']['speed']} m/s")
